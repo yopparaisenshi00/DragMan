662812b4a89aef170f45b10254daa6e98ffa5ef4
Yu-suke <recruitimaoka@gmail.com>
2019-09-04 14:06:19 +0900

    yusuke_commit

--- Assets/Effect/Script/BombEffectController.cs
+++ Assets/Effect/Script/BombEffectController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BombEffectController : MonoBehaviour
 {
+    private Color init_col;
+
     private Vector3 front;
 
     // エフェクトが動く速さ
@@ -21,7 +23,10 @@ public class BombEffectController : MonoBehaviour
     private int state;              // 状態
     private int timer_scale_up;     // 拡大する時間
     public int timer_scale_up_max;  // 何秒拡大するか
-    public float scale_spd;         // エフェクトが小さくなる速さ    
+    public float scale_spd;         // エフェクトが小さくなる速さ  
+
+    // 透明になる速さ
+    public float alpha_spd;  
 
     // 待機時間
     private int timer;
@@ -30,11 +35,16 @@ public class BombEffectController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // モデルの色
+        init_col = gameObject.GetComponent<MeshRenderer>().material.color;
+
+        // 初期の大きさをランダムにする
         float rand_scale = Random.Range(init_scale_min, init_scale_max);
+        transform.localScale = new Vector3(rand_scale, rand_scale, rand_scale);
+
+        // 初期の角度
         float rand_rot = Random.Range(init_rot_min, init_rot_max);
         float rand_x_rot = Random.Range(0, 180);
-
-        transform.localScale = new Vector3(rand_scale, rand_scale, rand_scale);
         transform.Rotate(new Vector3(rand_x_rot, rand_rot, rand_rot));
 
         // 角度を保存
@@ -45,7 +55,7 @@ public class BombEffectController : MonoBehaviour
     void Update()
     {
         Move();
-        Scale_Change();
+        Scale_Change();        
         Destroy();
     }
 
@@ -53,8 +63,6 @@ public class BombEffectController : MonoBehaviour
     {
         // 飛ばす
         transform.position -= front * (spd * Time.deltaTime);
-        // 一定の角度で回転させる
-        //transform.Rotate(new Vector3(90, 90, 90) * Time.deltaTime);
     }
 
     void Scale_Change()
@@ -66,17 +74,22 @@ public class BombEffectController : MonoBehaviour
                 transform.localScale -= new Vector3(scale_spd, scale_spd, scale_spd);
 
                 // 拡大終了
-                if (timer_scale_up++ > timer_scale_up_max)
-                {
-                    state = 1;
-                }
+                if (timer_scale_up++ > timer_scale_up_max) state = 1;
                 break;
             case 1:
                 // 徐々に小さく
                 transform.localScale += new Vector3(scale_spd, scale_spd, scale_spd);
+                alpha_change();
                 break;
         }
+    }
 
+    // 徐々に透明にする
+    void alpha_change()
+    {
+        // 徐々に透明にする
+        init_col.a -= alpha_spd;
+        gameObject.GetComponent<MeshRenderer>().material.color = init_col;
     }
 
     // 壊れる条件
@@ -93,6 +106,12 @@ public class BombEffectController : MonoBehaviour
             Destroy(gameObject);
         }
         if (transform.localScale.x > 4.1f)
+        {
+            Destroy(gameObject);
+        }
+
+        // 透明になったら消す
+        if(init_col.a < 0.0f)
         {
             Destroy(gameObject);
         }
---
+++ Assets/Scenes/yusuke_test/Assets/Scripts/GameManagerScore.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScore : MonoBehaviour
+{
+    public Text score_text;
+    public RectTransform rect;
+
+    public int text_size;                       // 大きさ
+    private int init_text_size;                 // 初期大きさ
+    private Vector3 text_pos;                   // 位置
+    private Vector3 init_text_pos;              // 初期位置
+    public int add_text_pos_x, add_text_pos_y;  // 足す分
+    public int sub_text_pos_x, sub_text_pos_y;  // 引く分
+
+    public int score;                           // 総得点
+    public int add_score;                       // スコアをどれだけ足すか
+    private bool calculation_score_fg;          // 計算をするかどうか
+
+    private int state;                          // テキスト大きくするときの状態
+    public int size_up_max;                     // サイズの上限
+    public int add_text_size;                   // スコアを足すときの数
+    public int sub_text_size;                   // 引くとき
+    private bool size_chenge_fg;                // サイズを変える時のフラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // テキストのサイズを設定
+        score_text.fontSize = init_text_size = text_size;
+
+        // 初期位置を設定
+        text_pos = init_text_pos = rect.transform.localPosition;
+
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // テキスト表示
+        text_set();
+
+        // スコアが入る動作（テスト）
+        test_calculation_score();
+
+        // サイズ変えてないときは初期サイズに戻す
+        size_reset();
+
+        // 位置リセット
+        pos_reset();
+        //pos_update();
+
+        // 計算する
+        if (calculation_score_fg) calculation_score(add_score);
+
+        // サイズを変える
+        if (size_chenge_fg)
+        {
+            size_chenge();
+            text_move_pos(add_text_pos_x, add_text_pos_y, sub_text_pos_x, sub_text_pos_y);    // 位置も更新
+        }
+    }
+
+    // テキスト表示
+    void text_set()
+    {
+        score_text.text = "スコア : " + score;
+    }
+
+    // 計算
+    void test_calculation_score()
+    {
+        // 計算していないときに何かしらの動作があったら計算出来る
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 計算をする
+            calculation_score_fg = true;
+
+            // 新しくスコアが入ったときに大きさをリセット
+            size_chenge_fg = false;
+
+            // 小さくなってる途中やったら初期化してから大きくなる
+            if (state == 1) state = 0;
+        }
+    }
+
+    // スコアの計算
+    void calculation_score(int add_score)
+    {
+        // 送られてきたスコアを代入
+        score += add_score;
+
+        // サイズを変える
+        size_chenge_fg = true;
+
+        // 計算を終わらせる
+        calculation_score_fg = false;
+    }
+
+    // テキストの大きさを変える
+    void size_chenge()
+    {
+        switch (state)
+        {
+            case 0:
+                // 画面に大きく表示
+                text_size += add_text_size;
+
+                // 拡大終了
+                if (text_size > size_up_max) state = 1;
+                break;
+            case 1:
+                // 元の大きさに戻す
+                text_size -= sub_text_size;
+
+                // 元のサイズに戻ったら処理を終わる
+                if (text_size <= init_text_size)
+                {
+                    state = 0;
+                    size_chenge_fg = false;
+                }
+                break;
+        }
+        score_text.fontSize = text_size;
+    }
+
+    // サイズをリセット
+    void size_reset()
+    {
+        // サイズの初期化
+        if (!size_chenge_fg) text_size = init_text_size;
+    }
+
+    //位置の更新
+    void text_move_pos(float add_pos_x, float add_pos_y, float sub_pos_x, float sub_pos_y)
+    {
+        if (state == 0) text_pos = new Vector3(text_pos.x + add_pos_x, text_pos.y - add_pos_y, 0);
+        if (state == 1) text_pos = new Vector3(text_pos.x - sub_pos_x, text_pos.y + sub_pos_y, 0);
+        rect.transform.localPosition = text_pos;
+    }
+
+    // 位置リセット
+    void pos_reset()
+    {
+        if (!size_chenge_fg) text_pos = init_text_pos;
+    }
+
+    // 桁が上がったら位置を更新
+    //void pos_update()
+    //{
+    //    rect.transform.localPosition = new Vector3(text_pos.x + (score / 10), text_pos.y, 0);
+    //}
+
+}
